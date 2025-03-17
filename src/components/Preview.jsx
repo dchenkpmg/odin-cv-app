d2cc7539336534d418f4957cef3998a390ca7906
dchenkpmg <dylanchen1@kpmg.co.nz>
2025-03-18 11:41:19 +1300

    feat: add preview sections

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -6,7 +6,21 @@ export default function Preview({ personal, education, experience }) {
       <div className="personal-preview">
         <div className="left-preview">
           <h2>{personal.name}</h2>
+          <p>{personal.email}</p>
+          <p>{personal.phone}</p>
+          <p>{personal.address}</p>
         </div>
+        <div className="right-preview">
+          <p>{personal.bio}</p>
+        </div>
+      </div>
+      <div className="experience">
+        <h2>Experience</h2>
+        {experience.map((job, index) => (
+          <div key={index} className="job">
+            <h3>{experience[index].job}</h3>
+          </div>
+        ))}
       </div>
     </div>
   );
